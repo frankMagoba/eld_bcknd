74f64e80b58a951ec87feed8b3fbc5f5ff2dbcb6
frankmagoba <franklinmagoba4@gmail.com>
2025-03-26 01:03:15 +0300

    fix deployement error

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,21 @@
 """
 WSGI config for eld_log project.
 
-This module redirects to the actual WSGI application in the eld_log package.
+This module contains the WSGI application for Render deployment.
 """
 
 import os
 import sys
 
-# Add the project directory to the Python path
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if path not in sys.path:
-    sys.path.append(path)
+# Add the parent directory to the Python path
+current_path = os.path.dirname(os.path.abspath(__file__))
+# Make sure the current directory is in the path
+if current_path not in sys.path:
+    sys.path.insert(0, current_path)
 
-# Import the WSGI application from the actual location
-from eld_log.eld_log.wsgi import application 
+# Set the Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eld_log.settings')
+
+# Import Django's WSGI handler
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application() 
